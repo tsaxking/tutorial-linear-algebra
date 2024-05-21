30c7bf4df0d965f8f633bdf778ad70f403c2964f
tsaxking <taylorking206@u.boisestate.edu>
2024-05-20 19:33:37 -0600

    add instructions;

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+// Write a program that handles points on a 2D plane.
+
+// Define a class Point with the following properties:
+// x: number
+// y: number
+
+// Add some methods to the Point class:
+// scale(factor: number): Point
+// add(other: Point): Point

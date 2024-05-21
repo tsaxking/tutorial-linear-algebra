361549943dc5dd6cfc0b36e4cb696fe575e6c864
tsaxking <taylorking206@u.boisestate.edu>
2024-05-20 19:36:56 -0600

    boilerplate

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,12 @@
 // Add some methods to the Point class:
 // scale(factor: number): Point
 // add(other: Point): Point
+
+
+class Point {
+}
+
+// const p1 = new Point(1, 2);
+// const p2 = new Point(3, 4);
+// const p3 = p1.add(p2).scale(2); // => Point { x: 8, y: 12 }
+// console.log(p3);
